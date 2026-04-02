02d8de0f56d82d77479f7b73153778c38108130e
Krabik_356 <bereza3622@gmail.com>
2026-04-02 10:07:40 +0300

    Виправив баг з базою даних

--- backend/db.sql
+++ backend/db.sql
@@ -1,3 +1,3 @@
-CREATE TABLE IF NOT EXISTS users(id SERIAL PRIMARY KEY, name TEXT, password TEXT, session_token TEXT, tocken_time TEXT,  rank INTEGER);
+CREATE TABLE IF NOT EXISTS users(id SERIAL PRIMARY KEY, name TEXT, password TEXT, session_token TEXT, token_time TEXT,  rank INTEGER);
 INSERT INTO users(name, password, rank) VALUES('Bodya', '12345', 100);
 INSERT INTO users(name, password, rank) VALUES('Anatoliy', 'asdad', 10);
